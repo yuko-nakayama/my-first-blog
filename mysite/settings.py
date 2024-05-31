c6073b35fe20911a2bf8bfd03835de5d1e495c2f
yuko <yuk.nakayama@icloud.com>
2024-05-31 13:00:42 +0900

    add whitenoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,10 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATICFILES_DIRS = (str(BASE_DIR.joinpath(’static’)),)
 STATIC_URL = '/static/'
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage’
-# STATIC_ROOT = BASE_DIR / 'static'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
